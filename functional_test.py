79f29bfe5c7eaf2a2fdacd0c70258085bcbf82b4
gkzz <gakuji.tamaki@gmail.com>
2019-03-02 00:37:41 +0900

    modify functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -1,27 +1,45 @@
+# soruces;
+# How do you test that a Python function throws an exception?
+# https://stackoverflow.com/questions/129507/how-do-you-test-that-a-python-function-throws-an-exception?rq=1
+
+
 import sys
 import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        EXECUTABLE_PATH ='/mnt/c/workspace/django-tdd/install/chromedriver.exe'
+
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-infobars")
+        self.driver = webdriver.Chrome(
+          executable_path = EXECUTABLE_PATH,
+          chrome_options=chrome_options,
+        )
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        LOCAL_HOST_URL = 'http://127.0.0.1:8000/'
+
+        self.driver.get(LOCAL_HOST_URL)
+        try:
+            #self.assertIn('To-Do', self.driver.title)
+            self.assertIn('Django', self.driver.title)
+        #except Exception as e:
+        except AssertionError as AE:
+            self.fail('Unexpected exception raised:', AE)
+        else:
+            self.fail('ExpectedException not raised')
 
 
-EXECUTABLE_PATH ='/mnt/c/workspace/django-tdd/install/chromedriver.exe'
-LOCAL_HOST_URL = ' http://127.0.0.1:8000/'
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
-start = time.time()
-chrome_options = Options()
-chrome_options.add_argument("--disable-infobars")
 
-driver = webdriver.Chrome(
-  executable_path = EXECUTABLE_PATH,
-  chrome_options=chrome_options,
-)
-driver.get(LOCAL_HOST_URL)
-
-assert 'Django' in driver.title
-
-end = time.time()
-print("process {0} ms".format((end - start) * 1000))
-sys.exit()
-
-driver.quit()
