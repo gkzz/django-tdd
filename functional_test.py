6a37055b043399f072a3e725c4bb8fde43355ca4
gkzz <gakuji.tamaki@gmail.com>
2019-03-07 00:49:46 +0900

    try-except & self.fail

--- functional_test.py
+++ functional_test.py
@@ -9,37 +9,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import unittest
-<<<<<<< HEAD
-
-class NewVisitorTest(unittest.TestCase):
-
-    def setUp(self):
-        EXECUTABLE_PATH ='/mnt/c/workspace/django-tdd/install/chromedriver.exe'
-
-        chrome_options = Options()
-        chrome_options.add_argument("--disable-infobars")
-        self.driver = webdriver.Chrome(
-          executable_path = EXECUTABLE_PATH,
-          chrome_options=chrome_options,
-        )
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        LOCAL_HOST_URL = 'http://127.0.0.1:8000/'
-
-        self.driver.get(LOCAL_HOST_URL)
-        try:
-            self.assertIn('To-Do', self.driver.title)
-            #self.assertIn('Django', self.driver.title)
-        except:
-            self.fail('fail the test')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-=======
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -63,15 +32,9 @@ class NewVisitorTest(unittest.TestCase):
         try:
             #self.assertIn('To-Do', self.driver.title)
             self.assertIn('Django', self.driver.title)
-        #except Exception as e:
-        except AssertionError as AE:
-            self.fail('Unexpected exception raised:', AE)
-        else:
-            self.fail('ExpectedException not raised')
+        except:
+            self.fail('fail the test')
 
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
-
->>>>>>> chapter2-feature-unittest-exception
