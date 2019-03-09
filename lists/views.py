727f92bf03490504fe117e636216c312a0b69fb7
gkzz <gakuji.tamaki@gmail.com>
2019-03-09 16:05:44 +0900

    modify lists/tests.py, views.py

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 #home_page = None
 
 def home_page(request):
-    pass
+#    pass
+     return HttpResponse()
