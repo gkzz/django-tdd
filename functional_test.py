ad427a3797efe355981c7f434f77fc350ed4ea5c
gkzz <gakuji.tamaki@gmail.com>
2019-03-11 08:19:37 +0900

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -33,6 +33,31 @@ class NewVisitorTest(unittest.TestCase):
         try:
             #self.assertIn('To-Do', self.driver.title)
             self.assertIn('Django', self.driver.title)
+            header_text = self.driver.find_element_by_tag_name('h1').text
+            self.assertIn('To-Do', header_text)
+
+            # She is invited to enter a to-do item straight away
+            inputbox = self.driver.find_element_by_id('id_new_item')
+            self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+            )
+
+            # She types "Buy peacock feathers" into a text box (Edith's hobby
+            # is tying fly-fishing lures)
+            inputbox.send_keys('Buy peacock feathers')
+
+            # When she hits enter, the page updates, and now the page lists
+            # "1: Buy peacock feathers" as an item in a to-do list table
+            inputbox.send_keys(Keys.ENTER)
+            time.sleep(1)
+
+            table = self.driver.find_element_by_id('id_list_table')
+            rows = table.find_elements_by_tag_name('tr')
+            self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows),
+                "New to-do item did not appear in table"
+            )
         except:
             self.fail('fail the test')
         
