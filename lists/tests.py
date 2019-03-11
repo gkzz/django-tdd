5d68b52dc7307682269216a3a6ccf5434cc4bec4
gkzz <gakuji.tamaki@gmail.com>
2019-03-11 23:00:00 +0900

    self.assertTemplateUsed(response, 'home.html') passed

--- lists/tests.py
+++ lists/tests.py
@@ -10,20 +10,20 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
     
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
            
         try:
             self.assertTrue( html.startswith('<!DOCTYPE html>'))
             #self.assertEqual('<title>Django-tdd</title>', html)
+            self.assertTrue(html.strip().endswith('</html>'))
+            self.assertTemplateUsed(response, 'home.html')
         except:
             self.fail('Finish the test!')
     
 
     def test_home_page_returns_correct_html_for_loop(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
 
         test_args_list = [
