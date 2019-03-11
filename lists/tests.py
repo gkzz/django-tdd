ed62fca3079d5f9601cf395e31af9f52266057ae
gkzz <gakuji.tamaki@gmail.com>
2019-03-12 00:12:43 +0900

    lists/tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,42 +1,7 @@
-from django.urls import resolve 
-from django.test import TestCase 
-from django.http import HttpRequest 
-from lists.views import home_page
-
+from django.test import TestCase
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-    
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-           
-        try:
-            self.assertTrue( html.startswith('<!DOCTYPE html>'))
-            #self.assertEqual('<title>Django-tdd</title>', html)
-            self.assertTrue(html.strip().endswith('</html>'))
-            self.assertTemplateUsed(response, 'home.html')
-        except:
-            self.fail('Finish the test!')
-    
 
-    def test_home_page_returns_correct_html_for_loop(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-        html = response.content.decode('utf8')
-
-        test_args_list = [
-            # self.assertIn(X, Y) check "Y in X"
-            ("<!DOCTYPE html>", html),  
-            #("<html><title>Error", html),
-            ("<title>Django-tdd", html),
-            #("<html><title>Django-tdd</title></html>", html),
-            #("<html><title>Django-tdd</title></html><body><h1>This is error!</h1></body>", html),
-        ]
-        
-        for x,  y in test_args_list:
-            with self.subTest(x=x, y=y):
-                self.assertIn(x, y)
-            
-        
+        self.assertTemplateUsed(response, 'home.html')
