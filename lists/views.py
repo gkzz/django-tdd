c6961e5917d336f5571352079f861a03ab56a479
gkzz <gakuji.tamaki@gmail.com>
2019-03-15 00:36:58 +0900

    self.assertIn('A new list item', response.content.decode())

--- lists/views.py
+++ lists/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def home_page(request):
     return render( request, 'home.html')
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    if request.method == 'POST': 
+        return HttpResponse(request.POST['item_text'])
+    
+    return render(request, 'home.html')
