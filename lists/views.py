3d6c6d6b6681b6f1d3affd702b4dc73ea15dc335
gkzz <gakuji.tamaki@gmail.com>
2019-03-17 00:09:56 +0900

    tests.py passed

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,10 @@
+"""
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
     return render( request, 'home.html')
-
+"""
 
 from django.http import HttpResponse
 from django.shortcuts import render
