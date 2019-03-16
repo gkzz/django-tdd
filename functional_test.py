1abf151b45d489462d9bb0f2c89eef4c111df807
gkzz <gakuji.tamaki@gmail.com>
2019-03-17 01:16:27 +0900

    No.01635

--- functional_test.py
+++ functional_test.py
@@ -38,29 +38,17 @@ class NewVisitorTest(unittest.TestCase):
 
             # She is invited to enter a to-do item straight away
             inputbox = self.driver.find_element_by_id('id_new_item')
-            self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
-            )
-
-            # She types "Buy peacock feathers" into a text box (Edith's hobby
-            # is tying fly-fishing lures)
-            inputbox.send_keys('Buy peacock feathers')
-
-            # When she hits enter, the page updates, and now the page lists
-            # "1: Buy peacock feathers" as an item in a to-do list table
+            inputbox.send_keys('Use peacock feathers to make a fly')
             inputbox.send_keys(Keys.ENTER)
             time.sleep(1)
 
             table = self.driver.find_element_by_id('id_list_table')
             rows = table.find_elements_by_tag_name('tr')
-            # self.assertTrue(
-            #     any(row.text == '1: Buy peacock feathers' for row in rows),
-            #     "New to-do item did not appear in table"
-            # )
-            self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows), 
-                f" New to-do item did not appear in table. Contents were:\ n{ table.text}" 
+            self.assertIn(
+                '1: Buy peacock feathers', [row.text for row in rows]
+            )
+            self.assertIn(
+                '2: Use peacock feathers to make a fly', [row.text for row in rows]
             )
         except:
             self.fail('fail the test')
