8131d11d3a0a1607a3af590aa83aabdb053092f4
gkzz <gakuji.tamaki@gmail.com>
2019-03-17 15:33:43 +0900

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,9 @@
 from django.db import models 
+
+#class Item(models.Model):
+#    pass
+
 class Item(models.Model):
-    pass
+    #text = models.TextField()
+    text = models.TextField(default ='')
+    
