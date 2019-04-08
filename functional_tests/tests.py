63083dbfc11345f25deec004eedc872e34393a63
gkzz <gakuji.tamaki@gmail.com>
2019-04-08 22:53:50 +0900

    NoSuchElementException, self.wait_for_row_in_list_table('1: Buy peacock feathers'

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -15,8 +15,11 @@ import unittest
 from django.test import LiveServerTestCase 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 
 
@@ -37,18 +40,50 @@ class NewVisitorTest(LiveServerTestCase):
         self.driver.quit()
 
     
-    def check_for_row_in_list_table(self, row_text):
-        try:
-            table = self.browser.find_element_by_id('id_list_table') 
-            rows = table.find_elements_by_tag_name('tr') 
-            self.assertIn(row_text, [row.text for row in rows])
-        except:
-            self.fail('fail the test')
-
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.driver.find_element_by_id('id_list_table') 
+                rows = table.find_elements_by_tag_name('tr') 
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.driver.get(self.live_server_url)
+        try:
+            self.driver.get(self.live_server_url)
 
+            # She is invited to enter a to-do item straight away
+            inputbox = self.driver.find_element_by_id('id_new_item')
+            self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+            )
+
+            # She types "Buy peacock feathers" into a text box (Edith's hobby
+            # is tying fly-fishing lures)
+            inputbox.send_keys('Buy peacock feathers')
+
+
+            # When she hits enter, the page updates, and now the page lists
+            # "1: Buy peacock feathers" as an item in a to-do list table
+            inputbox.send_keys(Keys.ENTER)
+            self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+            inputbox = self.driver.find_element_by_id('id_new_item')
+            inputbox.send_keys('Use peacock feathers to make a fly')
+            inputbox.send_keys(Keys.ENTER)
+
+            self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+            self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+            #self.fail('Finished, and successful!')
+        except:
+            self.fail('Failed.')
 
 
 if __name__ == '__main__':
