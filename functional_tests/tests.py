6b8a09cc64e0b0bb6faf30b1ed6aed22ab1aad5c
gkzz <gakuji.tamaki@gmail.com>
2019-04-12 22:04:20 +0900

    No2641

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,7 +44,7 @@ class NewVisitorTest(LiveServerTestCase):
         start_time = time.time()
         while True:
             try:
-                table = self.driver.find_element_by_id('id_nothing') 
+                table = self.driver.find_element_by_id('id_list_table')
                 rows = table.find_elements_by_tag_name('tr') 
                 self.assertIn(row_text, [row.text for row in rows])
                 return
@@ -53,7 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         try:
             self.driver.get(self.live_server_url)
 
@@ -84,7 +84,40 @@ class NewVisitorTest(LiveServerTestCase):
             #self.fail('Finished, and successful!')
         except:
             self.fail('Failed.')
+    
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        try:
+            self.driver.get(self.live_server_url)
+            inputbox = self.driver.find_element_by_id('id_new_item') 
+            inputbox.send_keys('Buy peacock feathers') 
+            inputbox.send_keys(Keys.ENTER) 
+            self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
+            edith_list_url = self.driver.current_url
+            self.assertRegex(edith_list_url, '/lists/.+')
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+            self.driver.quit()
+        except:
+            self.fail('Failed.')
+
+        
+        try:
+            self.setup()
+            self.driver.get(self.live_server_url)
+            page_text = self.driver.find_element_by_tag_name('body').text
+            self.assertNotIn('Buy peacock feathers', page_text)
+            self.assertNotIn('make a fly', page_text)
+            inputbox = self.driver.find_element_by_id('id_new_item')
+            inputbox.send_keys('Buy milk')
+            inputbox.send_keys(Keys.ENTER)
+            self.wait_for_row_in_list_table('1: Buy milk')
+            francis_list_url = self.driver.current_url
+            self.assertRegex( francis_list_url, '/lists/. +') 
+            self.assertNotEqual(francis_list_url, edith_list_url)
+
+            page_text = self.driver.find_element_by_tag_name('body').text
+            self.assertNotIn('Buy peacock feathers', page_text)
+            self.assertIn('Buy milk', page_text)
+        except:
+            self.fail('Failed.')
+
