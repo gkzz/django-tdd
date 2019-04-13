a7083b93cab26242ef6e49baf1101e1b39308a0a
gkzz <gakuji.tamaki@gmail.com>
2019-04-14 00:36:07 +0900

    No3209, functional_tests/tests.py, line 139, in test_layout_and_styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -128,3 +128,13 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=10
         )
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.driver.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
