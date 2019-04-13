8e7b84324b30dc12305b4d97f32430e40f9441ea
gkzz <gakuji.tamaki@gmail.com>
2019-04-13 21:21:40 +0900

    hotfix

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,41 +85,5 @@ class NewVisitorTest(LiveServerTestCase):
             #self.fail('Finished, and successful!')
         except:
             self.fail('Failed.')
-    """
-    def test_multiple_users_can_start_lists_at_different_urls(self):
-        try:
-            self.driver.get(self.live_server_url)
-            inputbox = self.driver.find_element_by_id('id_new_item') 
-            inputbox.send_keys('Buy peacock feathers') 
-            inputbox.send_keys(Keys.ENTER) 
-            self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-            edith_list_url = self.driver.current_url
-            self.assertRegex(edith_list_url, '/lists/.+')
-
-            self.driver.quit()
-        except:
-            self.fail('Failed.')
-
-        
-        try:
-            self.setup()
-            self.driver.get(self.live_server_url)
-            page_text = self.driver.find_element_by_tag_name('body').text
-            self.assertNotIn('Buy peacock feathers', page_text)
-            self.assertNotIn('make a fly', page_text)
-            inputbox = self.driver.find_element_by_id('id_new_item')
-            inputbox.send_keys('Buy milk')
-            inputbox.send_keys(Keys.ENTER)
-            self.wait_for_row_in_list_table('1: Buy milk')
-            francis_list_url = self.driver.current_url
-            self.assertRegex( francis_list_url, '/lists/. +') 
-            self.assertNotEqual(francis_list_url, edith_list_url)
-
-            page_text = self.driver.find_element_by_tag_name('body').text
-            self.assertNotIn('Buy peacock feathers', page_text)
-            self.assertIn('Buy milk', page_text)
-        except:
-            self.fail('Failed.')
-    """
 
