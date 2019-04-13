c151acc89cb000b8594c7e9ef961a83b43a208ba
gkzz <gakuji.tamaki@gmail.com>
2019-04-14 00:30:48 +0900

    No3210

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -116,3 +116,15 @@ class NewVisitorTest(LiveServerTestCase):
             self.assertIn('Buy milk', page_text)
         except:
             self.fail('Failed.')
+    
+    def test_layout_and_styling(self):
+        self.driver.get(self.live_server_url)
+        self.driver.set_window_size(1024, 768)
+
+
+        inputbox = self.driver.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
